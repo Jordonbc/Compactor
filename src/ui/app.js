338e834e57bb79e0d053d9d873b05d062873a7d3
Jordon Brooks <jordonbc@hotmail.co.uk>
2024-10-09 02:56:48 +0100

    Kinda works

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,238 +1,241 @@
 /* jshint strict: true, esversion: 5, browser: true */
 
 var Util = (function() {
-	"use strict";
+    "use strict";
 
-	var powers = '_KMGTPEZY';
-	var monotime = function() { return Date.now(); };
+    var powers = '_KMGTPEZY';
+    var monotime = function() { return Date.now(); };
 
-	if (window.performance && window.performance.now)
-		monotime = function() { return window.performance.now(); };
+    if (window.performance && window.performance.now)
+        monotime = function() { return window.performance.now(); };
 
-	return {
-		debounce: function(callback, delay) {
-			var timeout;
-			var fn = function() {
-				var context = this;
-				var args = arguments;
+    return {
+        debounce: function(callback, delay) {
+            var timeout;
+            var fn = function() {
+                var context = this;
+                var args = arguments;
 
-				clearTimeout(timeout);
-				timeout = setTimeout(function() {
-					timeout = null;
-					callback.apply(context, args);
-				}, delay);
-			};
-			fn.clear = function() {
-				clearTimeout(timeout);
-				timeout = null;
-			};
+                clearTimeout(timeout);
+                timeout = setTimeout(function() {
+                    timeout = null;
+                    callback.apply(context, args);
+                }, delay);
+            };
+            fn.clear = function() {
+                clearTimeout(timeout);
+                timeout = null;
+            };
 
-			return fn;
-		},
+            return fn;
+        },
 
-		throttle: function(callback, delay) {
-			var timeout;
-			var last;
-			var fn = function() {
-				var context = this;
-				var args = arguments;
-				var now = monotime();
+        throttle: function(callback, delay) {
+            var timeout;
+            var last;
+            var fn = function() {
+                var context = this;
+                var args = arguments;
+                var now = monotime();
 
-				if (last && now < last + delay) {
-					clearTimeout(timeout);
-					timeout = setTimeout(function() {
-						timeout = null;
-						last = now;
-						callback.apply(context, args);
-					}, delay);
-				} else {
-					last = now;
-					callback.apply(context, args);
-				}
-			};
-			fn.clear = function() {
-				clearTimeout(timeout);
-				timeout = null;
-			};
+                if (last && now < last + delay) {
+                    clearTimeout(timeout);
+                    timeout = setTimeout(function() {
+                        timeout = null;
+                        last = now;
+                        callback.apply(context, args);
+                    }, delay);
+                } else {
+                    last = now;
+                    callback.apply(context, args);
+                }
+            };
+            fn.clear = function() {
+                clearTimeout(timeout);
+                timeout = null;
+            };
 
-			return fn;
-		},
+            return fn;
+        },
 
-		format_number: function(number, digits) {
-			if (digits === undefined) digits = 2;
-			return number.toLocaleString("en", {minimumFractionDigits: digits, maximumFractionDigits: digits});
-		},
+        format_number: function(number, digits) {
+            if (digits === undefined) digits = 2;
+            return number.toLocaleString("en", {minimumFractionDigits: digits, maximumFractionDigits: digits});
+        },
 
-		bytes_to_human_dec: function(bytes) {
-			for (var i = powers.length - 1; i > 0; i--) {
-				var div = Math.pow(10, 3 * i);
-				if (bytes >= div) {
-					return Util.format_number(bytes / div, 2) + " " + powers[i] + 'B';
-				}
-			}
+        bytes_to_human_dec: function(bytes) {
+            for (var i = powers.length - 1; i > 0; i--) {
+                var div = Math.pow(10, 3 * i);
+                if (bytes >= div) {
+                    return Util.format_number(bytes / div, 2) + " " + powers[i] + 'B';
+                }
+            }
 
-			return Util.format_number(bytes, 0) + ' B';
-		},
+            return Util.format_number(bytes, 0) + ' B';
+        },
 
-		bytes_to_human_bin: function(bytes) {
-			for (var i = powers.length - 1; i > 0; i--) {
-				var div = Math.pow(2, 10*i);
-				if (bytes >= div) {
-					return Util.format_number(bytes / div, 2) + " " + powers[i] + 'iB';
-				}
-			}
+        bytes_to_human_bin: function(bytes) {
+            for (var i = powers.length - 1; i > 0; i--) {
+                var div = Math.pow(2, 10 * i);
+                if (bytes >= div) {
+                    return Util.format_number(bytes / div, 2) + " " + powers[i] + 'iB';
+                }
+            }
 
-			return Util.format_number(bytes, 0) + ' B';
-		},
+            return Util.format_number(bytes, 0) + ' B';
+        },
 
-		human_to_bytes: function(human) {
-			if (!human) return null;
-			var num = parseFloat(human);
+        human_to_bytes: function(human) {
+            if (!human) return null;
+            var num = parseFloat(human);
 
-			var match = (/\s*([KMGTPEZY])(i)?([Bb])?\s*$/i).exec(human);
-			if (match) {
-				var pow = (match[2] == 'i') ? 1024 : 1000;
-				// var mul = (match[3] == 'B') ? 8 : 1;
+            var match = (/\s*([KMGTPEZY])(i)?([Bb])?\s*$/i).exec(human);
+            if (match) {
+                var pow = (match[2] == 'i') ? 1024 : 1000;
 
-				num *= Math.pow(pow, powers.indexOf(match[1].toUpperCase()));
-			}
+                num *= Math.pow(pow, powers.indexOf(match[1].toUpperCase()));
+            }
 
-			return num;
-		},
+            return num;
+        },
 
-		number_to_human: function(num) {
-			for (var i = powers.length - 1; i > 0; i--) {
-				var div = Math.pow(10, 3*i);
-				if (num >= div) {
-					return Util.format_number(num / div, 2) + powers[i];
-				}
-			}
+        number_to_human: function(num) {
+            for (var i = powers.length - 1; i > 0; i--) {
+                var div = Math.pow(10, 3 * i);
+                if (num >= div) {
+                    return Util.format_number(num / div, 2) + powers[i];
+                }
+            }
 
-			return num;
-		},
+            return num;
+        },
 
-		human_to_number: function(human) {
-			if (!human) return null;
-			var num = parseFloat(human);
+        human_to_number: function(human) {
+            if (!human) return null;
+            var num = parseFloat(human);
 
-			var match = (/\s*([KMGTPEZY])\s*$/i).exec(human);
+            var match = (/\s*([KMGTPEZY])\s*$/i).exec(human);
 
-			if (match) {
-				num *= Math.pow(1000, powers.indexOf(match[1].toUpperCase()));
-			}
+            if (match) {
+                num *= Math.pow(1000, powers.indexOf(match[1].toUpperCase()));
+            }
 
-			return num;
-		},
+            return num;
+        },
 
-		sformat: function() {
-			var args = arguments;
-			return args[0].replace(/\{(\d+)\}/g, function (m, n) { return args[parseInt(n, 10) + 1]; });
-		},
+        sformat: function() {
+            var args = arguments;
+            return args[0].replace(/\{(\d+)\}/g, function (m, n) { return args[parseInt(n, 10) + 1]; });
+        },
 
-		range: function(a, b, step) {
-			if (!step) step = 1;
-			var arr = [];
-			for (var i = a; i < b; i += step) {
-				arr.push(i);
-			}
-			return arr;
-		}
-	};
+        range: function(a, b, step) {
+            if (!step) step = 1;
+            var arr = [];
+            for (var i = a; i < b; i += step) {
+                arr.push(i);
+            }
+            return arr;
+        }
+    };
 })();
 
 Util.bytes_to_human = Util.bytes_to_human_bin;
 
 // Actions call back into Rust
 var Action = (function() {
-	"use strict";
+    "use strict";
 
-	return {
-		open_url: function(url) {
-			external.invoke(JSON.stringify({ type: 'OpenUrl', url: url }));
-		},
+    return {
+        open_url: function(url) {
+            external.invoke(JSON.stringify({ type: 'OpenUrl', url: url }));
+        },
 
-		reset_config: function() {
-			external.invoke(JSON.stringify({ type: 'ResetConfig' }));
-		},
+        reset_config: function() {
+            external.invoke(JSON.stringify({ type: 'ResetConfig' }));
+        },
 
-		save_config: function(config) {
-			config.type = 'SaveConfig';
-			external.invoke(JSON.stringify(config));
-		},
+        save_config: function(config) {
+            config.type = 'SaveConfig';
+            external.invoke(JSON.stringify(config));
+        },
 
-		choose_folder: function() {
-			external.invoke(JSON.stringify({ type: 'ChooseFolder' }));
-		},
+        choose_folder: function() {
+            external.invoke(JSON.stringify({ type: 'ChooseFolder' }));
+        },
 
-		compress: function() {
-			external.invoke(JSON.stringify({ type: 'Compress' }));
-		},
+        compress: function() {
+            external.invoke(JSON.stringify({ type: 'Compress' }));
+        },
 
-		decompress: function() {
-			external.invoke(JSON.stringify({ type: 'Decompress' }));
-		},
+        decompress: function() {
+            external.invoke(JSON.stringify({ type: 'Decompress' }));
+        },
 
-		pause: function() {
-			external.invoke(JSON.stringify({ type: 'Pause' }));
-		},
+        pause: function() {
+            external.invoke(JSON.stringify({ type: 'Pause' }));
+        },
 
-		resume: function() {
-			external.invoke(JSON.stringify({ type: 'Resume' }));
-		},
+        resume: function() {
+            external.invoke(JSON.stringify({ type: 'Resume' }));
+        },
 
-		analyse: function() {
-			external.invoke(JSON.stringify({ type: 'Analyse' }));
-		},
+        analyse: function() {
+            external.invoke(JSON.stringify({ type: 'Analyse' }));
+        },
 
-		stop: function() {
-			external.invoke(JSON.stringify({ type: 'Stop' }));
-		},
+        stop: function() {
+            external.invoke(JSON.stringify({ type: 'Stop' }));
+        },
 
-		quit: function() {
-			external.invoke(JSON.stringify({ type: 'Quit' }));
-		}
-	};
+        quit: function() {
+            external.invoke(JSON.stringify({ type: 'Quit' }));
+        }
+    };
 })();
 
 // Responses come from Rust
 var Response = (function() {
-	"use strict";
+    "use strict";
 
-	return {
-		dispatch: function(msg) {
-			switch(msg.type) {
-				case "Config":
-					Gui.set_decimal(msg.decimal);
-					Gui.set_compression(msg.compression);
-					Gui.set_excludes(msg.excludes);
-					break;
+    return {
+        dispatch: function(msg) {
+            switch (msg.type) {
+                case "Config":
+                    Gui.set_decimal(msg.decimal);
+                    Gui.set_compression(msg.compression);
+                    Gui.set_excludes(msg.excludes);
+                    break;
 
-				case "Folder":
-					Gui.set_folder(msg.path);
-					break;
+                case "Folder":
+                    Gui.set_folder(msg.path);
+                    break;
 
-				case "Version":
-					Gui.version(msg.date, msg.version);
-					break;
+                case "Version":
+                    Gui.version(msg.date, msg.version);
+                    break;
 
-				case "Status":
-					Gui.set_status(msg.status, msg.pct);
-					break;
+                case "Status":
+                    Gui.set_status(msg.status, msg.pct);
+                    break;
 
-				case "Paused":
-				case "Resumed":
-				case "Stopped":
-				case "Scanned":
-				case "Compacting":
-					Gui[msg.type.toLowerCase()]();
-					break;
+                case "Paused":
+                case "Resumed":
+                case "Stopped":
+                case "Scanned":
+                case "Compacting":
+                    Gui[msg.type.toLowerCase()]();
+                    break;
 
-				case "FolderSummary":
-					Gui.set_folder_summary(msg.info);
-					break;
-			}
-		}
-	};
+                case "FolderSummary":
+                    Gui.set_folder_summary(msg.info);
+                    break;
+
+                case "Page":
+                    Gui.page(msg.page);
+                    break;
+            }
+        }
+    };
 })();
 
 // Anything poking the GUI lives here
@@ -299,7 +302,7 @@ var Gui = (function() {
             button.empty();
             bits.forEach(function(bit) {
                 button.append(bit);
-                button.append($("<span>❱</span>"));
+                button.append($("<span>❯</span>"));
             });
             button.append(end);
 
@@ -374,9 +377,9 @@ var Gui = (function() {
             Gui.set_folder_summary({
                 logical_size: 0,
                 physical_size: 0,
-                compressed: {count: 0, logical_size: 0, physical_size: 0},
-                compressible: {count: 0, logical_size: 0, physical_size: 0},
-                skipped: {count: 0, logical_size: 0, physical_size: 0}
+                compressed: { count: 0, logical_size: 0, physical_size: 0 },
+                compressible: { count: 0, logical_size: 0, physical_size: 0 },
+                skipped: { count: 0, logical_size: 0, physical_size: 0 }
             });
         },
 
@@ -400,34 +403,26 @@ var Gui = (function() {
             $("#File_Count_Compressible").text(Util.format_number(data.compressible.count, 0));
             $("#File_Count_Skipped").text(Util.format_number(data.skipped.count, 0));
 
+            // Update legend values
+            $("#Legend_Compressed").text(Util.bytes_to_human(data.compressed.physical_size) + " in compressed");
+            $("#Legend_Compressible").text(Util.bytes_to_human(data.compressible.physical_size) + " in compressible");
+            $("#Legend_Skipped").text(Util.bytes_to_human(data.skipped.physical_size) + " in excluded");
+            $("#Legend_Saved").text(Util.bytes_to_human(data.compressed.logical_size - data.compressed.physical_size) + " saved");
+
             // Calculate the widths of the breakdown sections
             if (data.logical_size > 0) {
                 var total = data.logical_size;
-                var compressedWidth = (100 * (data.compressed.physical_size / total)).toFixed(2);
-                var compressibleWidth = (100 * (data.compressible.physical_size / total)).toFixed(2);
-                var skippedWidth = (100 * (data.skipped.physical_size / total)).toFixed(2);
-                var savedWidth = (100 * ((data.compressed.logical_size - data.compressed.physical_size) / total)).toFixed(2);
+
+                var compressedWidth = (data.compressed.physical_size / total) * 100;
+                var compressibleWidth = (data.compressible.physical_size / total) * 100;
+                var skippedWidth = (data.skipped.physical_size / total) * 100;
+                var savedWidth = ((data.compressed.logical_size - data.compressed.physical_size) / total) * 100;
 
                 // Update the breakdown bar sections
-                var compressedElement = document.getElementById("Breakdown_Compressed");
-                if (compressedElement) {
-                    compressedElement.style.width = compressedWidth + "%";
-                }
-
-                var compressibleElement = document.getElementById("Breakdown_Compressible");
-                if (compressibleElement) {
-                    compressibleElement.style.width = compressibleWidth + "%";
-                }
-
-                var skippedElement = document.getElementById("Breakdown_Skipped");
-                if (skippedElement) {
-                    skippedElement.style.width = skippedWidth + "%";
-                }
-
-                var savedElement = document.getElementById("Breakdown_Saved");
-                if (savedElement) {
-                    savedElement.style.width = savedWidth + "%";
-                }
+                $("#Breakdown_Compressed").css("width", compressedWidth + "%").text(Util.bytes_to_human(data.compressed.physical_size) + " in compressed");
+                $("#Breakdown_Compressible").css("width", compressibleWidth + "%").text(Util.bytes_to_human(data.compressible.physical_size) + " in compressible");
+                $("#Breakdown_Skipped").css("width", skippedWidth + "%").text(Util.bytes_to_human(data.skipped.physical_size) + " in excluded");
+                $("#Breakdown_Saved").css("width", savedWidth + "%").text(Util.bytes_to_human(data.compressed.logical_size - data.compressed.physical_size) + " saved");
             }
         },
 
